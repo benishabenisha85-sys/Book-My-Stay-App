0f492c204029704f5a52bc5e1c34e9092d85f306
benishabenisha85-sys <benishabenisha85@gmail.com>
2026-03-16 15:13:58 +0530

    UC1 Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,14 @@
 public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
+
+        // Inform user that application started successfully
+        System.out.println("Application started successfully!");
+    }
 }
+
